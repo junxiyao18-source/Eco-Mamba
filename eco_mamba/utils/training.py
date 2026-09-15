d02cd0b95eaf60b10c38541c3234ee6a97babc6a
junxiyao18-source <junxiyao18@gmail.com>
2026-09-15 17:35:39 +0800

    Add files via upload

--- eco_mamba/utils/training.py
+++ eco_mamba/utils/training.py
@@ -1,1 +1,17 @@
-11
+"""Small training utilities used by the Eco-Mamba trainer."""
+
+
+class AverageMeter:
+    """Track a weighted running average."""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = self.avg = self.sum = self.count = 0.0
+
+    def update(self, value, n=1):
+        self.val = value
+        self.sum += value * n
+        self.count += n
+        self.avg = self.sum / self.count
